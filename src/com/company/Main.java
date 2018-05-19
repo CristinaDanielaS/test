488b272794f0cc3a355ab44b49e3add6b7337f0c
RamonaMihaela <ramonat78@yahoo.com>
2018-05-19 13:40:31 +0300

    m modificat

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int x [] = {10,15,20,25,30};
-	System.out.println(x.toString());
+
+        int n = 5;
+        int sum = 0;
+        sum = sum +n;
+        for (int i = 0; i < n; i++) {
+            sum = sum + n;
+        }
+
+        System.out.println(sum);
     }
 }
